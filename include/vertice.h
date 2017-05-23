a85ef936d6c2458d1022354e20b1363ac2f7221f
Cristofer Oswald <cristoferoswald@gmail.com>
2017-05-23 19:07:42 -0300

    Terminado header de vertice

--- include/vertice.h
+++ include/vertice.h
@@ -13,10 +13,30 @@ enum Cor{
 
 class Vertice{
 public:
+  int getDescobrimento();
+  int getTermino();
+  int getDistancia();
+
+  std::vector<Vertice*> getVizinhos();
+  Cor getCor();
+  Vertice* getPrececessor();
+
+  void setDescobrimento(int descobrimento);
+  void setTermino(int termino);
+  void setDistancia(int distancia);
+  
+  void addVizinho(Vertice* vertice);
+  void setCor(Cor cor);
+  void setPrececessor(Vertice* vertice);
 private:
-  std::vector<Vertice*> vizinhos;
-  Cor cor = Cor::BRANCO;
-  Vertice* predecessor;
+  int descobrimento_ = 0;
+  int termino_ = 0;
+  int distancia_ = 0;
+
+  std::vector<Vertice*> vizinhos_;
+  Cor cor_ = Cor::BRANCO;
+  Vertice* predecessor_;
+
 };
 
 #endif //VERTICE_H
